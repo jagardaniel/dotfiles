09b0dba7796f531d01017c343266460f293da554
Daniel Matsson <jagardaniel@gmail.com>
2022-05-14 15:06:02 +0200

    Basic neovim configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.g.mapleader = ','
+vim.opt.pastetoggle = '<F2>'
