cb41f35631b24365551e70363493090390da8bf8
Daniel Matsson <jagardaniel@gmail.com>
2022-05-24 20:20:17 +0200

    Trying out some neovim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,13 @@
+-- Include plugins and plugin settings
+require('plugins')
+
+-- General options
 vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.termguicolors = true
 
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
@@ -7,6 +16,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 
+-- Keymaps
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.g.mapleader = ','
-vim.opt.pastetoggle = '<F2>'
