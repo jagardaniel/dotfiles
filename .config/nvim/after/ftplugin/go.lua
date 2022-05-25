dcc24d73d404bd2a53eb1424af322559505360af
Daniel Matsson <jagardaniel@gmail.com>
2022-05-25 17:06:42 +0200

    Modify tab settings in vim, suggested by "Vim Indentation Hell: A Best Effort Guide"

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,4 +1,3 @@
 vim.opt_local.expandtab = false
-vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 0
 vim.opt_local.softtabstop = 0
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,8 +13,9 @@ vim.opt.smartcase = true
 vim.opt.ignorecase = true
 
 vim.opt.expandtab = true
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = -1
 
 -- Keymaps
 vim.keymap.set('i', 'jj', '<Esc>')
