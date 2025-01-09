995e994bcfb617f55de7bda60fa82f7eb34c13bf
Daniel Matsson <jagardaniel@gmail.com>
2025-01-09 01:23:38 +0100

    Neovim config updates

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -21,8 +21,12 @@ return {
 
     vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Fzf files" })
     vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Fzf buffers" })
-    vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Fzf live grep" })
+    vim.keymap.set("n", "<leader>f/", fzf.live_grep, { desc = "Fzf live grep" })
     vim.keymap.set("n", "<leader>fh", fzf.helptags, { desc = "Fzf helptags" })
-    vim.keymap.set("n", "<leader>fc", function() fzf.files({ cwd = vim.fn.stdpath("config") }) end, { desc = "Fzf files (config)" })
+    vim.keymap.set("n", "<leader>fg", fzf.git_files, { desc = "Fzf git files" })
+    vim.keymap.set("n", "<leader>fo", fzf.oldfiles, { desc = "Fzf oldfiles" })
+    vim.keymap.set("n", "<leader>fc", function()
+      fzf.files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "Fzf files (config)" })
   end
 }
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,5 +4,7 @@ return {
     require("mini.icons").setup()
     require("mini.pairs").setup()
     require("mini.statusline").setup()
+    require("mini.git").setup()
+    require("mini.diff").setup()
   end
 }
